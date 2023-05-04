33eaa4f1c289999ee16693a6803530a421b6b67b
MorriganH <morriganhennessy@gmail.com>
2023-05-04 01:22:23 -0400

    refactors getUserWithId to get data from database

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -21,7 +21,7 @@ const getUserWithEmail = function (email) {
     .query(`
     SELECT *
       FROM users
-     WHERE email = $1
+     WHERE email = $1;
     `, [email])
     .then(res => res.rows[0])
     .catch(err => console.log(err.message));
@@ -33,7 +33,16 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`
+    SELECT *
+      FROM users
+     WHERE id = $1;
+     `, [id])
+     .then(res => res.rows[0])
+     .catch(err => console.log(err.message));
+
+  // return Promise.resolve(users[id]);
 };
 
 /**
